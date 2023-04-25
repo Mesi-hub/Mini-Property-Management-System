abd478ebc1d8b2603afcb240e8bc261b7e5eaac7
Sanjaya <sanjaya.jayasooriya@gmail.com>
2023-04-25 10:21:35 -0500

    Added Pages and Routes

---
+++ src/spa/src/pages/HomePage/index.js
@@ -0,0 +1,11 @@
+import Banner from "../../components/Banner";
+import Houses from "../../components/Houses";
+
+export const HomePage = () => {
+  return (
+    <>
+      <Banner />
+      <Houses />
+    </>
+  );
+};

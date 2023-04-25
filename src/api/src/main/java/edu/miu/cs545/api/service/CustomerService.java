c192d4480d1a71be3737795f5c2f7307041fce51
Mesi <yihunm@gmail.com>
2023-04-25 08:53:02 -0500

    Revert "implemented Check offer History"
    This reverts commit f6cb4c3609cf5826c873df8997eeb93dbbc83779.

--- src/api/src/main/java/edu/miu/cs545/api/service/CustomerService.java
+++ src/api/src/main/java/edu/miu/cs545/api/service/CustomerService.java
@@ -1,7 +1,6 @@
 package edu.miu.cs545.api.service;
 
 import edu.miu.cs545.api.dto.CustomerDto;
-import edu.miu.cs545.api.dto.OfferDto;
 import edu.miu.cs545.api.entity.Person;
 
 import java.util.List;
@@ -11,5 +10,4 @@ public interface CustomerService {
     CustomerDto findById(long id);
     boolean deleteById(long id);
     boolean save(CustomerDto customer);
-    List<OfferDto> findOffersByCustomerId(long customerId);
 }

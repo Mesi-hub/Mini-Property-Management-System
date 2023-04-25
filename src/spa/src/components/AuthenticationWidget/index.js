d2034ad4aa7c666e081d7cc7a64e60b447c01588
Sanjaya <sanjaya.jayasooriya@gmail.com>
2023-04-25 08:17:34 -0500

    Add authentication widget to Navbar (temporary)

--- src/spa/src/components/AuthenticationWidget/index.js
+++ src/spa/src/components/AuthenticationWidget/index.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { authenticateUser } from "../../services/api/authentication";
 import { authenticationSuccess } from "../../feature/Authentication/authenticationSlice";
 import { useDispatch, useSelector } from "react-redux";
@@ -7,14 +7,13 @@ import { userinfoSuccess } from "../../feature/Authentication/loggedinUserSlice"
 
 export const AuthenticationWidget = () => {
   const authenticationForm = useRef();
+  const [authenticationStoreValueChanged, setAuthenticationStoreValueChanged] = useState(false);
+  const [loggedinUserStoreValueChanged, setLoggedinUserStoreValueChanged] = useState(false);
   const dispatch = useDispatch();
-  const { authenticationStoreValue } = useSelector(
-    (state) => state.authentication
-  );
-  //const { loggedinUserStoreValue } = useSelector(state => state.loggedinUser);
+  const store = useSelector((state) => state);
   const authenticate = async (event) => {
     event.preventDefault();
-    console.log("Before Store Token:" + authenticationStoreValue?.token);
+    console.log("Before Store Token:" + store?.authentication?.value?.token);
     let authResult = await authenticateUser(
       authenticationForm.current["username"].value,
       authenticationForm.current["password"].value
@@ -24,16 +23,24 @@ export const AuthenticationWidget = () => {
     let userInfo = await getUserInfo();
     console.log("Returned UserInfo from API" + userInfo.name);
     dispatch(userinfoSuccess(userInfo));
+    setAuthenticationStoreValueChanged(true);
+    setLoggedinUserStoreValueChanged(true);
   };
   return (
-    <div>
-      <form ref={authenticationForm} onSubmit={authenticate}>
-        <input name="username"></input>
-        <br />
-        <input name="password"></input>
-        <br />
-        <button>Login</button>
-      </form>
-    </div>
+    <>
+      {store?.authentication?.value?.token ? (
+         <div className="nav-link active">
+         Welcome {store?.loggedinUser?.value?.name}
+       </div>
+      ) : (
+        <form ref={authenticationForm} onSubmit={authenticate}>
+          <input name="username"></input>
+          <br />
+          <input name="password"></input>
+          <br />
+          <button>Login</button>
+        </form>
+      )}
+    </>
   );
 };

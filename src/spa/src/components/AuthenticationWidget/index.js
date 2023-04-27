730906295b94123ccffad2a506ae8ef145183562
Sanjaya <sanjaya.jayasooriya@gmail.com>
2023-04-26 22:02:45 -0500

    Client Logoff

--- src/spa/src/components/AuthenticationWidget/index.js
+++ src/spa/src/components/AuthenticationWidget/index.js
@@ -1,8 +1,20 @@
-import { isLoggedIn } from "../../feature/Authentication/authenticationSlice";
-import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { isLoggedIn, logoff } from "../../feature/Authentication/authenticationSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutUser } from "../../services/api/authentication";
+import { logoffUserSuccess } from "../../feature/Authentication/loggedinUserSlice";
 
 export const AuthenticationWidget = () => {
   const store = useSelector((state) => state);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const logout = async () => {
+    await logoutUser();
+    await dispatch(logoff({}));
+    await dispatch(logoffUserSuccess({}));
+    navigate("/");
+
+  }
   return (
     <>
       {isLoggedIn(store) ? (
@@ -10,7 +22,7 @@ export const AuthenticationWidget = () => {
           <span className="badge text-bg-primary">
             {store?.loggedinUser?.value?.name}
           </span>
-          <button type="button" className="btn btn-link">
+          <button type="button" className="btn btn-link" onClick={logout}>
             Logout
           </button>
         </>
